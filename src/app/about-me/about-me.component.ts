fe0b07cb42c804e8f965f97451809933aa669295
Danny Libin <omegasol11@gmail.com>
2016-11-14 23:03:38 -0600

    feat(proficiencies): add easing animation to skillbars

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit
+} from '@angular/core';
 
 import { TransitionService } from '../shared/transition.service';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent implements OnInit {
   languages = [];
